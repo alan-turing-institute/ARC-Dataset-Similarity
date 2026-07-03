d9c240245aaed3adb90869de902f2042fb72ffa1
philswatton <philswatton@gmail.com>
2026-07-03 10:57:42 +0100

    save path

--- scripts/run_metrics.py
+++ scripts/run_metrics.py
@@ -80,8 +80,8 @@ def main(
         "dataset2": dataset2_name,
         **metrics_results,
     }
-    METRICS_RESULT_DIR.mkdir(parents=True, exist_ok=True)
     result_path = METRICS_RESULT_DIR / f"{cfg_name}.yaml"
+    result_path.parent.mkdir(parents=True, exist_ok=True)
     save_yaml_to_path(results, result_path)
 
     print(f"Results saved to {result_path}")
