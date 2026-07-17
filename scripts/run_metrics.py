60370c2665c7d54cf6dc98a5ad0b0fc6c4413b28
philswatton <philswatton@gmail.com>
2026-07-17 10:48:55 +0100

    passing labels to data store

--- scripts/run_metrics.py
+++ scripts/run_metrics.py
@@ -66,10 +66,18 @@ def main(
     metrics_list: list[str],
     dataset1_name: str,
     dataset2_name: str,
+    copy_label_scheme: bool = False,
 ) -> None:
     # Instantiate datasets
     print("Loading datasets")
     ds1 = load_dataset_from_config(dataset1_cfg)
+    if copy_label_scheme:
+        label_to_class_map = {
+            label: cat for cat, label in ds1.class_to_label_map.items()
+        }
+        dataset2_cfg["kwargs"]["positive_class"] = [
+            label_to_class_map[label] for label in ds1.positive_class
+        ]
     ds2 = load_dataset_from_config(dataset2_cfg)
     print("Datasets loaded successfully")
 
